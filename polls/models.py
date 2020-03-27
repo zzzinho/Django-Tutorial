4c4bc376298e0cb00d64c5b2f7b6df7ff1d7f7f9
zzzinho <59327026+zzzinho@users.noreply.github.com>
2020-03-27 19:01:38 +0900

    modified:   README.md 	modified:   db.sqlite3 	modified:   mysite/__pycache__/settings.cpython-37.pyc 	modified:   mysite/settings.py 	new file:   polls/__pycache__/admin.cpython-37.pyc 	new file:   polls/__pycache__/apps.cpython-37.pyc 	new file:   polls/__pycache__/models.cpython-37.pyc 	modified:   polls/admin.py 	new file:   polls/migrations/0001_initial.py 	new file:   polls/migrations/__pycache__/0001_initial.cpython-37.pyc 	new file:   polls/migrations/__pycache__/__init__.cpython-37.pyc 	modified:   polls/models.py

--- polls/models.py
+++ polls/models.py
@@ -1,10 +1,36 @@
 from django.db import models
+from django.utils import timezone
+import datetime
 
 # Create your models here.
-
-class Quesetion(models.Model):
-    question_text = models.CharField(manx_length=200)
+"""
+    CharField: 문자 필드
+    DateTimeField: 날짜와 시간(datetime)
+"""
+class Question(models.Model):
+    question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+
+    def __str__(self):
+        return self.question_text
+    
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
 
 class Choice(models.Model):
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    choice_text = models.CharField(max_length=200)
+    votes = models.IntegerField(default=0)
+    
+    def __str__(self):
+        return self.choice_text
+    
+"""
+    makemigrations를 실행해서, 도델을 변경시킨 사실과 이 변경사항을 
+    migration으로 저장시키고 싶다는 것을 Django에게 알려준다.
+
+    모델의 바꾸는 법
+    1. models.py에서 모델을 변경
+    2. python3 manage.py makemigrations를 통해 변경사항에 대한 migration생성
+    3. python3 manage.py migrate 명령을 통해 변경사항을 데이터베이스에 적용
+"""
